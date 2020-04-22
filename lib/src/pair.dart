893df78f57d11a67360758543aceb7efdff0e246
Michał Sikora <me@mehow.io>
2020-04-22 19:58:29 +0200

    Add documentation to Pair type.

--- lib/src/pair.dart
+++ lib/src/pair.dart
@@ -1,4 +1,5 @@
 import 'package:meta/meta.dart';
+import 'package:missile/src/utils.dart';
 
 /// A tuple of two values.
 @immutable
@@ -14,13 +15,29 @@ class Pair<T1, T2> {
   /// Creates a new pair with two values.
   const factory Pair(T1 first, T2 second) = Pair<T1, T2>._;
 
-  Pair<R, T2> mapFirst<R>(R Function(T1) mapper) => Pair(mapper(first), second);
+  /// Maps the first value of this [Pair] and returns a new one with the second value copied.
+  Pair<R, T2> mapFirst<R>(R Function(T1) mapper) {
+    requireNotNull(mapper);
+    return Pair(mapper(first), second);
+  }
 
-  Pair<T1, R> mapSecond<R>(R Function(T2) mapper) => Pair(first, mapper(second));
+  /// Maps the second value of this [Pair] and returns a new one with the first value copied.
+  Pair<T1, R> mapSecond<R>(R Function(T2) mapper) {
+    requireNotNull(mapper);
+    return Pair(first, mapper(second));
+  }
 
-  R transform<R>(R Function(Pair<T1, T2>) transformer) => transformer(this);
+  /// Applies the [transformer] to this [Pair].
+  R transform<R>(R Function(Pair<T1, T2>) transformer) {
+    requireNotNull(transformer);
+    return  transformer(this);
+  }
 
-  R transformValues<R>(R Function(T1, T2) transformer) => transformer(first, second);
+  /// Applies the [transformer] to values of this [Pair].
+  R transformValues<R>(R Function(T1, T2) transformer) {
+    requireNotNull(transformer);
+    return transformer(first, second);
+  }
 
   @override
   String toString() => 'Pair($first,$second)';
